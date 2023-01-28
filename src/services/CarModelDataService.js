163e5c298e03e1de8e7f7ef5e0a43f023db1acfb
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-01-28 18:54:17 +0300

    add new car & carmodel

---
+++ src/services/CarModelDataService.js
@@ -0,0 +1,26 @@
+import http from '../http-common';
+
+class CarModelDataService{
+
+    get(id){
+        return http.get(`/carmodels/${id}`);
+    }
+
+    getAll(){
+        return http.get("/carmodels");
+    }
+
+    create(data){
+        return http.post('/carmodels', data);
+    }
+
+    update(id, data){
+        return http.put(`/carmodels/${id}`, data);
+    }
+
+    delete(id){
+        return http.delete(`/carmodels/${id}`);
+    }
+};
+
+export default new CarModelDataService();
